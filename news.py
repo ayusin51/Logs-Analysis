c2f691680a3b8fe25e7b3332b5c73016729eda23
ayusin51 <ayushsingla51@gmail.com>
2018-04-20 23:13:11 +0530

    Check with PEP8

--- news.py
+++ news.py
@@ -26,28 +26,30 @@ ques1['title'] = "1. The most popular three articles of all time :"
 ques2['title'] = "2. The most popular article authors of all time :"
 ques3['title'] = "3. Days when the requests lead to an error more than 1% :"
 
+
 def run_query(query):
-	db = psycopg2.connect(database=dbname)
-	c = db.cursor()
-	c.execute(query)
-	result = c.fetchall()
-	db.close()
-	return result
+    db = psycopg2.connect(database=dbname)
+    c = db.cursor()
+    c.execute(query)
+    result = c.fetchall()
+    db.close()
+    return result
+
 
 def print_res(ques):
-	print('\n' + ques['title'] + '\n\n')
-	for row in ques['result']:
-		print(str(row[0]) + ' : ' + str(row[1]) + '\n')
-	print('\n')
+    print('\n' + ques['title'] + '\n\n')
+    for row in ques['result']:
+        print(str(row[0]) + ' : ' + str(row[1]) + '\n')
+    print('\n')
 
 if __name__ == '__main__':
-	# running the queries
-	ques1['result'] = run_query(query1)
-	ques2['result'] = run_query(query2)
-	ques3['result'] = run_query(query3)
-	
-	# print result of queries
-	print_res(ques1)
-	print_res(ques2)
-	print_res(ques3)
-	
+    # running the queries
+    ques1['result'] = run_query(query1)
+    ques2['result'] = run_query(query2)
+    ques3['result'] = run_query(query3)
+
+    # print result of queries
+    print_res(ques1)
+    print_res(ques2)
+    print_res(ques3)
+
